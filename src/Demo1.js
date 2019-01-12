f7dd7b3825df32596013898daa410692588e32ba
brady <acheng_69@163.com>
2019-01-12 16:49:43 +0800

    React

---
+++ src/Demo1.js
@@ -0,0 +1,95 @@
+/**
+ * Created by brady on 2019/1/12.
+ */
+import React, { Component } from 'react';
+import './Demo1.css';
+import Demo2 from './Demo2';
+
+//对象形式样式
+let styleDiv = {background: '#ff0'};
+
+/**
+ * 组件生命周期：
+ * 创建阶段 - Mounting
+ * 运行阶段 - Updating
+ * 卸载阶段 - Unmounting
+ * */
+
+class Demo1 extends Component {
+  constructor(props) {
+    super(props)
+    this.names = ['Alice', 'Emily', 'Kate']
+    this.username = 'Brady'
+    this.age = '32'
+    this.state = {
+      sex: 'male',
+      count: props.initCount
+    }
+  }
+  /**
+   * 组件被挂载到页面之前调用，其在render()之前被调用，因此在这方法里同步地设置状态将不会触发重渲染
+   * 特点：
+   * 1, 该阶段的函数只执行一次
+   * 注意：
+   * 1, 无法获取页面中的DOM对象
+   * 用途：
+   * 1, 可以调用setState()方法来改变状态值
+   * 2, 可以发送Ajax请求获取数据
+   * */
+  componentWillMount() {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  /**
+   * 渲染组件到页面中，无法获取页面中的DOM对象
+   * 注意：
+   * 1, 不要在render方法中调用 setState() 方法，否则会递归渲染（状态改变会重新调用render()，render()又重新改变状态）
+   * */
+  render() {
+    return (
+      <div style={styleDiv}>
+        {this.state.count}
+        <Demo2 username={this.username} age={this.age} sex={this.state.sex}/>
+        <ul style={{fontSize:'16px',color:'#ff4040'}}>
+          {
+            this.names.map((name, index) => {
+              return <li className="Demo1-li" key={index}>Hello, {name}</li>
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+
+  /**
+   * 组件已经挂载到页面中
+   * 特点：
+   * 1, 该阶段的函数只执行一次
+   * 注意：
+   * 1, 在这里修改状态会重新渲染
+   * 用途：
+   * 1, 可以操作DOM，获取到组件内部的DOM对象
+   * 2, 可以发送Ajax请求获取数据
+   * 3, 可以调用setState()方法来改变状态值
+   * */
+  componentDidMount() {
+    console.log(this.state.count)
+  }
+
+  /**
+   * 只要组件不再被渲染到页面中，那么这个方法就会被调用
+   * 用途：
+   * 1,
+   * */
+  componentWillUnmount() {
+  }
+}
+
+//组件props初始化
+Demo1.defaultProps = {
+  initCount: 0
+};
+
+export default Demo1
---
+++ src/Demo2.js
@@ -0,0 +1,16 @@
+/**
+ * Created by brady on 2019/1/12.
+ */
+import React, { Component } from 'react';
+
+class Demo2 extends Component {
+  render() {
+    return (
+      <div>
+        <p>姓名：{this.props.username}，年龄：{this.props.age}，性别：{this.props.sex}</p>
+      </div>
+    );
+  }
+}
+
+export default Demo2
