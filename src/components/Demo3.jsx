bcc226c10d39acb6ec54ad66bdc83a3632e81af0
Brady <acheng_69@163.com>
2019-01-17 20:25:40 +0800

    React Demo

---
+++ src/components/Demo3.jsx
@@ -0,0 +1,17 @@
+/**
+ * Created by brady on 2019-01-13.
+ */
+
+import React, {Component} from 'react';
+
+class Demo3 extends Component {
+  render() {
+    return (
+      <div>
+        <p>姓名：{this.props.username}，年龄：{this.props.age}，性别：{this.props.sex}</p>
+      </div>
+    )
+  }
+}
+
+export default Demo3 
