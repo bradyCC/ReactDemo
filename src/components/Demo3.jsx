ad8c0b5d4bb208e58bd5cc3f31d6aa457338942a
brady <acheng_69@163.com>
2019-01-18 18:01:37 +0800

    React Demo

--- src/components/Demo3.jsx
+++ src/components/Demo3.jsx
@@ -8,9 +8,14 @@ class Demo3 extends Component {
   render() {
     return (
       <div>
+        <button onClick={this.myclickHandler}>按钮</button>
         <p>姓名：{this.props.username}，年龄：{this.props.age}，性别：{this.props.sex}</p>
       </div>
     )
+  }
+
+  myclickHandler () {
+    console.log('11111')
   }
 }
 
